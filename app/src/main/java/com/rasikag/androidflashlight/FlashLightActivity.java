c3573947e1bbdb624cd807985cd0b1bf68f75ef6
Rasika Gayan <rasikagayan90@gmail.com>
2015-12-02 14:44:23 -0800

    completed with basic functions

--- app/src/main/java/com/rasikag/androidflashlight/FlashLightActivity.java
+++ app/src/main/java/com/rasikag/androidflashlight/FlashLightActivity.java
@@ -1,52 +1,166 @@
 package com.rasikag.androidflashlight;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ImageButton;
 
 public class FlashLightActivity extends AppCompatActivity {
+
+    private ImageButton mSwich;
+    private boolean isFlashOn;
+    private boolean isDeviceHasFlas;
+
+
+    private Camera mCamera;
+    private Camera.Parameters mParams;
+    private MediaPlayer mMediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flash_light);
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
-    }
+        mSwich = (ImageButton) findViewById(R.id.btnSwitch);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_flash_light, menu);
-        return true;
-    }
+        isDeviceHasFlas = getApplicationContext().getPackageManager()
+                .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (!isDeviceHasFlas) {
+            AlertDialog alert = new AlertDialog.Builder(FlashLightActivity.this)
+                    .create();
+            alert.setTitle("Error");
+            alert.setMessage("Sorry, your device doesn't support flash light!");
+            alert.setButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            });
+            alert.show();
+            return;
         }
 
-        return super.onOptionsItemSelected(item);
+        getCamera();
+
+        toggleButtonImage();
+
+        mSwich.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isFlashOn) {
+                    turnOffFlash();
+                } else {
+                    turnOnFlash();
+                }
+            }
+        });
+
+    }
+
+    private void getCamera() {
+        if (mCamera == null) {
+            try {
+                mCamera = Camera.open();
+                mParams = mCamera.getParameters();
+            } catch (RuntimeException e) {
+                Log.e("Failed to Open. Error: ", e.getMessage());
+            }
+        }
+    }
+
+    private void turnOnFlash() {
+
+        if (!isFlashOn) {
+            if (mCamera == null || mParams == null) {
+                return;
+            }
+            playSound();
+
+            mParams = mCamera.getParameters();
+            mParams.setFlashMode(mParams.FLASH_MODE_TORCH);
+            mCamera.setParameters(mParams);
+            mCamera.startPreview();
+            isFlashOn = true;
+
+            toggleButtonImage();
+        }
+
+    }
+
+    private void turnOffFlash() {
+        if (isFlashOn) {
+            if (mCamera == null || mParams == null) {
+                return;
+            }
+            playSound();
+
+            mParams = mCamera.getParameters();
+            mParams.setFlashMode(mParams.FLASH_MODE_OFF);
+            mCamera.setParameters(mParams);
+            mCamera.stopPreview();
+            isFlashOn = false;
+
+            toggleButtonImage();
+        }
+    }
+
+    private void toggleButtonImage() {
+        if (isFlashOn) {
+            mSwich.setImageResource(R.drawable.btn_switch_on);
+        } else {
+            mSwich.setImageResource(R.drawable.btn_switch_off);
+        }
+    }
+
+    private void playSound() {
+        if (isFlashOn) {
+            mMediaPlayer = MediaPlayer.create(FlashLightActivity.this, R.raw.light_switch_off);
+        } else {
+            mMediaPlayer = MediaPlayer.create(FlashLightActivity.this, R.raw.light_switch_on);
+        }
+        mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.release();
+            }
+
+        });
+        mMediaPlayer.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        turnOffFlash();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (isDeviceHasFlas) {
+            turnOnFlash();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        getCamera();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mCamera != null) {
+            mCamera.release();
+            mCamera = null;
+        }
     }
 }
